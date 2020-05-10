c608b903f2af37d9951f9ddd5e94db619f47c9b8
takahashi <shigeki.takahashi.dev@gmail.com>
2020-05-11 01:50:11 +0900

    rom引き数修正

--- sample.py
+++ sample.py
@@ -22,14 +22,15 @@ from pyboy import PyBoy
 # コマンドライン引数
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    "--filepath", help='gb file path', default='00.ROMs/01.hello_world.gb')
+    "--rom", help='gb file path', default='00.ROMs/01.hello_world.gb')
 args = parser.parse_args()
 
-filepath = args.filepath
+rom_path = args.rom
+print('ROM : ' + rom_path)
 
 # PyBoy起動
 color_palette = [0xd6e895, 0xacc04c, 0x527d3e, 0x264a2e]
-pyboy = PyBoy(filepath, color_palette=color_palette)
+pyboy = PyBoy(rom_path, color_palette=color_palette)
 
 while not pyboy.tick():
     pass
